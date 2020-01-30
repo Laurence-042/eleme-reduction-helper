ec046178484c88bf99b7c66238d9228d07392d5f
Laurence042 <qq2120286535@gmail.com>
2020-01-30 13:14:28 +0800

    simplify food data

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
-import io
-from urllib import request
 import gzip
+import io
 import json
 import os
+from urllib import request
+
+
+class Food:
+    name = None
+    category = None
+    price = 0.0
+    packing_fee = 0.0
+    second_price = 0.0
+    applicable_quantity = 0
+
+    def __init__(self, name, category, price, packing_fee, second_price, applicable_quantity):
+        self.name = name
+        self.category = category
+        self.price = price
+        self.packing_fee = packing_fee
+        self.second_price = second_price
+        self.applicable_quantity = applicable_quantity
+
 
 shop_code = "E7320367095212271912"
 data_dir = "./" + shop_code + ".json"
@@ -32,14 +50,31 @@ data = json.loads(html)
 for activity_tag in data['rst']['activity_tags']:
     print(activity_tag['text'])
 
+foods = []
+
 for menu_entry in data['menu']:
-    print("====" + menu_entry['name'] + "====")
-    for food in menu_entry['foods']:
-        print(food['name'], food['lowest_price'], end=' ')
-        if len(food['specfoods']) != 1:
+    category = menu_entry['name']
+    print("====" + category + "====")
+
+    for raw_food in menu_entry['foods']:
+        if len(raw_food['specfoods']) != 1:
             pass  # TODO: what if amount of specfoods larger than 1?
-        for spec in food['specfoods']:
-            if spec['original_price'] is not None:
-                print("原价", spec['original_price'], end=' ')
-                print("限{0}份".format(spec['activity']['applicable_quantity']), end=' ')
-            print("餐盒费", spec['packing_fee'])
+
+        raw_food = raw_food['specfoods'][0]
+
+        name = raw_food['name']
+        second_price = raw_food['price']
+        price = second_price
+        packing_fee = raw_food['packing_fee']
+        applicable_quantity = 1
+        if raw_food['original_price'] is not None:
+            price = raw_food['original_price']
+            applicable_quantity = raw_food['activity']['applicable_quantity']
+        food = Food(name=name, category=category, price=price, packing_fee=packing_fee, second_price=second_price,
+                    applicable_quantity=applicable_quantity)
+        foods.append(food)
+        print(food.name, food.second_price, end=' ')
+
+        print("原价", food.price, end=' ')
+        print("限{0}份".format(food.applicable_quantity), end=' ')
+        print("餐盒费", food.packing_fee)
